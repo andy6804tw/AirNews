9ca6c1db54a58866e74e729549bb98f293fe3d55
andy6804tw <andy6804tw@yahoo.com.tw>
2017-07-02 13:58:39 +0800

    新增DataModel並寫入list，爬蟲優化

--- app/src/main/java/com/openweather/airnews/MainActivity.java
+++ app/src/main/java/com/openweather/airnews/MainActivity.java
@@ -4,19 +4,25 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import com.openweather.airnews.DataModel.DataModel;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private Document document;
+    public ArrayList<DataModel> list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        list=new ArrayList<DataModel>();
     }
     @Override
     protected void onResume() {
@@ -29,27 +35,26 @@ public class MainActivity extends AppCompatActivity {
                     document = Jsoup.connect("http://e-info.org.tw/taxonomy/term/258/all")
                             .timeout(3000)
                             .get();
-                    Elements noteList = document.select("div.view-content");
-                    Elements title = noteList.select("div.views-field.views-field-title");
-                    Elements Image = noteList.select("div.views-field.views-field-field-image").select("img");
-                    Elements detail = noteList.select("div.views-field.views-field-body");
-                    Elements time = noteList.select("span.views-field.views-field-created");
-                    int c=0,c2=0,c3=0,c4=0;
+                    Elements noteList = document.select("div").select("#block-system-main").select("div.views-row");
+                    //Log.e("Data",noteList.toString());
+                    int c=0;
                     //Log.e("Name"+c++,Image.attr("abs:src"));
-                   for (Element element:title) {
-                         String url=element.select("a").attr("abs:href");
-                        String Title=element.text();
-                        Log.e("Title"+c++,url+" "+Title);
+                    for (Element element:noteList){
+                        Elements title = element.select("div.views-field.views-field-title");
+                        Elements url =element.select("div.views-field.views-field-title").select("a");
+                        Elements Image = element.select("div.views-field.views-field-field-image").select("img");
+                        Elements detail = element.select("div.views-field.views-field-body");
+                        Elements time = element.select("span.views-field.views-field-created");
+                        //Log.e("Title"+c++,title.text()+" "+Image.attr("abs:src")+" "+detail.text()+" "+time.text()+" "+url.attr("abs:href"));
+                        list.add(new DataModel(title.text(),time.text(),detail.text(),Image.attr("abs:src"),url.attr("abs:href")));
                     }
-                    for(Element element:Image){
-                        Log.e("img"+c2++,element.attr("abs:src"));
-                    }
-                    for(Element element:detail){
-                        Log.e("detail"+c3++,element.text());
-                    }
-                    for(Element element:time){
-                        Log.e("time"+c4++,element.text());
-                    }
+                   for(int i =0;i<list.size();i++){
+                       Log.e("Title"+i,list.get(i).getTitle());
+                       Log.e("time"+i,list.get(i).getTime());
+                       Log.e("detail"+i,list.get(i).getDetail());
+                       Log.e("Image"+i,list.get(i).getImage());
+                       Log.e("url"+i,list.get(i).getUrl());
+                   }
 
                 } catch (IOException e) {
                     e.printStackTrace();
